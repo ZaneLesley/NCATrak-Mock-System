4f5d02734bc2d7b2151b862cf6f8c6129dee1f8d
ZaneLesley <zanelesley@yahoo.com>
2024-09-06 18:08:57 -0500

    Refactored code as well as improved readability
    I made this script a while ago, so just did some small improvements and
    formatted the code to look better and be easier to read.

--- data-generator.py
+++ data-generator.py
@@ -1,5 +1,5 @@
 """
-An Example on how data generation works. Also Using this to test the Requirements.txt file.
+An example on how data generation works. Also Using this to test the Requirements.txt file.
 and installation of everything.
 """
 
@@ -7,29 +7,35 @@ from faker import Faker
 import pandas as pd
 from rich import print
 
-def generate_data(n):
+
+def generate_data(n) -> pd.DataFrame:
     data = []
     fake = Faker()
-    Faker.seed(0)
-    for i in range (n):
+    fake.seed_instance(0)
+    for _ in range(n):
         person = {}
-        person['cust_id'] = fake.unique.random_int(min = 0, max=9999)
-        if fake.random_int(min = 0, max = 1) == 0:
-            person['first_name'] = fake.first_name_male()
-            person['second_name'] = fake.first_name_male()
-            person['last_name'] = fake.last_name()
-            person['sex'] = "male"
+        person["cust_id"] = fake.unique.random_int(min=0, max=9999)
+
+        # Generates a male or female randomly.
+        if fake.random_int(min=0, max=1) == 0:
+            person["first_name"] = fake.first_name_male()
+            person["second_name"] = fake.first_name_male()
+            person["last_name"] = fake.last_name()
+            person["sex"] = "male"
         else:
-            person['first_name'] = fake.first_name_female()
-            person['second_name'] = fake.first_name_female()
-            person['last_name'] = fake.last_name()
-            person['sex'] = "female"
-        person['date_of_birth'] = fake.date_of_birth(minimum_age = 18, maximum_age = 100)
-        person['address'] = fake.address()
-        person['acct_no'] = fake.unique.random_int(min= 0, max= 100000)
-        person['acct_bal'] = fake.pydecimal(right_digits = 2, positive = True, min_value = 1, max_value = 10000)
-        person['acct_type'] = fake.random_int(min = 0, max = 1000, step = 100)
-        person['acct_opening_date'] = fake.date_of_birth(minimum_age = 0, maximum_age = 25)
+            person["first_name"] = fake.first_name_female()
+            person["second_name"] = fake.first_name_female()
+            person["last_name"] = fake.last_name()
+            person["sex"] = "female"
+
+        person["date_of_birth"] = fake.date_of_birth(minimum_age=18, maximum_age=100)
+        person["address"] = fake.address()
+        person["acct_no"] = fake.unique.random_int(min=0, max=100000)
+        person["acct_bal"] = fake.pydecimal(
+            right_digits=2, positive=True, min_value=1, max_value=10000
+        )
+        person["acct_type"] = fake.random_int(min=0, max=1000, step=100)
+        person["acct_opening_date"] = fake.date_of_birth(minimum_age=0, maximum_age=25)
         data.append(person)
     return pd.DataFrame(data)
 
@@ -41,5 +47,5 @@ if __name__ == "__main__":
     print("[yellow]Generating Data...")
     final_data = generate_data(n)
     print("[yellow]Writing .csv file...")
-    final_data.to_csv('data.csv')
+    final_data.to_csv("data.csv")
     print("[green]Data written to /data.csv")
