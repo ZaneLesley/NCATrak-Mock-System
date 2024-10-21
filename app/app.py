ad09f9b13047a9fa87190bb97c06ab8ff704b870
ZaneLesley <zanelesley@yahoo.com>
2024-10-21 10:30:27 -0500

    Created an example of interface linking
    I created a temporary example of how to do interface linking. This shows
    how you can use a manager application to switch between windows. It's
    important to note that all interfaces will have to change their __init__
    calls to match this, and all switching should be done by the manager
    application app.py

---
+++ app/app.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+import Generaltab_interface
+
+class tkinterApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        container = tk.Frame(self)
+        container.pack(side = "top", fill = "both", expand=True)
+        
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+        
+        self.frames = {}
+        
+        frame = Generaltab_interface.GeneraltabInterface(parent=container, controller=self)
+        self.frames[Generaltab_interface.GeneraltabInterface] = frame
+        
+        frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(Generaltab_interface.GeneraltabInterface)
+            
+    
+    def show_frame(self, frame_class):
+        frame = self.frames[frame_class]
+        frame.tkraise()
+
+if __name__ == "__main__":
+    app = tkinterApp()
+    app.geometry("1600x800")
+    app.mainloop()
