1236a8ec0442949d4550bdddcc38cfbacc4733d9
Zane Lesley <zanelesley@ou.edu>
2024-12-01 12:21:00 -0600

    continued work on fixing the file path problems in the data-generation

--- app/generator/util.py
+++ app/generator/util.py
@@ -19,9 +19,8 @@ def find_column(key: str, column: str, table: list, value: str) -> dict:
 def write_to_csv(data: list, name: str):
     df = pd.DataFrame(data)
 
-    generator_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "generator", "csvs")
+    generator_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "csvs")
     os.makedirs(generator_path, exist_ok=True)
     filename = os.path.join(generator_path, name + ".csv")
-    
-    df.to_csv(filename, index=False)    
+    df.to_csv(filename, index=False)
     print(f"[green]Successfully wrote data to {filename}.")
